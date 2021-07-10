ffacfb1a4c8beb942b0be4a7f26fc02a2a0bfaf6
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-07-10 19:14:29 -0300

    feat: add a clipboard copier to the button room-code

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -2,13 +2,25 @@ import copyImg from '../assets/images/copy.svg';
 
 import '../styles/room-code.scss';
 
-export function RoomCode() {
+type RoomCodeProps = {
+    code: string;
+}
+
+export function RoomCode({ code }: RoomCodeProps) {
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(code);
+    }
+
     return (
-        <button className="room-code">
+        <button
+            type="button"
+            className="room-code"
+            onClick={copyRoomCodeToClipboard}
+        >
             <div>
                 <img src={copyImg} alt="Copy room code" />
             </div>
-            <span>Sala #234o23487987</span>
+            <span>Sala #{code}</span>
         </button>
     );
 }
