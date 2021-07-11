68630a88f8b701a7d575f4f35ee4b4e77238b93c
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2021-07-11 19:12:42 -0300

    feat: add Question component

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,16 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import { database } from '../services/firebase';
+import { Question } from '../components/Question';
 
 import logoImg from '../assets/images/logo.svg';
 
 import '../styles/room.scss';
-import { useEffect } from 'react';
 
-type Question = {
+type QuestionType = {
     id: string;
     author: {
         name: string;
@@ -39,7 +39,7 @@ export function Room() {
     const { user } = useAuth();
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState('');
     const roomID = params.id;
 
@@ -122,7 +122,17 @@ export function Room() {
                     </div>
                 </form>
 
-                {JSON.stringify(questions)}
+                <div className="question-list">
+                    {questions.map(question => {
+                        return (
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            />
+                        );
+                    })}
+                </div>
             </main>
         </div>
     );
